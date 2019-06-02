ee164b9986e5bfe0928cdf0a1ab9169571c4f5cf
Austin Hill <hillam.mail@gmail.com>
2019-06-01 21:49:37 -0400

    fixed tests (I guess they weren't actually running the asserts), and got everything passing

--- src/my_collections/core.clj
+++ src/my_collections/core.clj
@@ -2,12 +2,14 @@
   (:gen-class))
 
 (defn first
+  "Return the first item in the given collection"
   [[head & tail]]
   head
 )
 
 (defn rest
+  "Return all but the first item from the given collection"
   [[head & tail]]
-  tail
+  (if (empty? tail) '() tail)
 )
 
--- test/my_collections/core_test.clj
+++ test/my_collections/core_test.clj
@@ -2,26 +2,43 @@
   (:require [clojure.test :refer :all]
             [my-collections.core :refer :all :as my]))
 
-; Vector of test collections
+; Vector of test cases
 (def test-colls [
   (range 1 10)
+  (seq (range 1 10))
+  (list (range 1 10))
+  (vector (range 1 10))
   (list "1" "2" "3")
+  {:a 1 :b 2}
+  1
+  "banana"
+  :definitely-a-collection
 ])
 
 (defn compare-fns
   "Test that the results of the two given functions are the same
   for all given collections."
   [my-fn core-fn coll]
-    (is (= (my-fn   coll)
+    (try
+      (core-fn coll)
+      (is
+        (= (my-fn coll)
            (core-fn coll)))
+      (catch Exception e
+        (is (thrown? Exception (my-fn coll)))))
 )
 
 (deftest first-test
-  (testing "my/first should always match clojure.core's"
-    (map (partial compare-fns my/first first) test-colls))
+  (doseq
+    [coll test-colls]
+    (testing "my/first should always match clojure.core's"
+      (compare-fns my/first clojure.core/first coll)))
 )
 
 (deftest rest-test
-  (testing "my/rest should always match clojure.core's"
-    (map (partial compare-fns my/rest rest) test-colls)))
+  (doseq
+    [coll test-colls]
+    (testing "my/rest should always match clojure.core's"
+      (compare-fns my/rest clojure.core/rest coll)))
+)
 
